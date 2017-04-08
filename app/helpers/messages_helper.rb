ca7bba067313ed25b2cb857476e37ca3ff8fce0c
konstantinsmirnov <k.smi@mail.ru>
2017-04-08 17:29:54 +0300

    Refactor mewssages index" " " "

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,10 +1,18 @@
 module MessagesHelper
-  
+
   def inbox_count(user)
     Message.where(recipient: user, is_read?: false).count
   end
 
   def message_status_icon(is_read)
     is_read == true ? 'circle-thin' : 'circle'
+  end
+
+  def render_text_with_tags(text)
+    sanitize text, tags: ['b', 'i', 'strong', 'a']
+  end
+
+  def render_text_preview(text)
+    truncate( render_text_with_tags(text), length: 100)
   end
 end
