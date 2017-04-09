77d7d222423c82b87ae5a765da458465060204b8
konstantinsmirnov <k.smi@mail.ru>
2017-04-09 18:25:19 +0300

    Update tags for message text

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -8,14 +8,14 @@ module MessagesHelper
     is_read == true ? 'circle-thin' : 'dot-circle-o'
   end
 
-  def render_text_with_tags(text)
-    sanitize text, tags: ['b', 'i', 'strong', 'a']
-  end
-
   def render_text_preview(text)
     truncate( render_text_with_tags(text), length: 100)
   end
 
+  def render_text_with_tags(text)
+    sanitize text, tags: ['b', 'i', 'strong', 'a']
+  end
+  
   def sortable(column, title = nil, params)
     title ||= column.titleize
     css_class = column == sort_column ? "current #{sort_direction}" : nil
